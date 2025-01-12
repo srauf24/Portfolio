c9a123f13c7733d7bbf317587790767387d2e2c6
Samee Rauf <samee.rauf@pmg.com>
2025-01-11 21:29:33 -0600

    added post hog in html

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@
 //
 // Scripts
 //
-import posthog from 'posthog-js';
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -32,10 +31,6 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-        posthog.init('phc_XNsaYYbePdiuOrRwEETGxAirNM5rUgdnMlLhHAlgjG1', {
-            api_host: 'https://us.i.posthog.com/',
-            person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-        })
 });
 // Initialize PostHog
 
